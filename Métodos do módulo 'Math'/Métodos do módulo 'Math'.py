49f25ab064174bb168d915d790f1683c8a33b6b1
Lucas Camargo <lucas8camargo@outlook.com>
2021-07-05 22:22:31 -0300

    Métodos do módulo 'Math'

---
+++ Métodos do módulo 'Math'/Métodos do módulo 'Math'.py
@@ -0,0 +1,15 @@
+Import math 
+ 
+math.floor(x, /) - Return the floor of xasan integral. 
+math.ceil(x, /) - Return the ceiling of xasan integral. 
+math.trunc(x, /) - Truncates the real x to the nearest integral toward0. 
+math.degrees(x, /) - Convert angle xfromradians to degrees. 
+math.exp(x, /) - Return e raised to the power of x. 
+math.factorial(x, /) - Find x!. Raise a ValueErrorifxisnegativeornon-integral. 
+math.pow(x, y, /) - Return x**y (x to the power of y). 
+math.radians(x, /) - Convert angle xfromdegrees to radians. 
+math.sqrt(x, /) - Return the square root of x. 
+math.log(x, [base=math.e]) - Return the logarithm of x to the given base. 
+math.sin(x, /) - Return the sine of x (measuredinradians). 
+math.cos(x, /) - Return the cosine of x (measuredinradians). 
+math.tan(x, /) - Return the tangent of x (measuredinradians).
