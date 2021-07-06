5e07ddf1bf09d5d172ec32b9b3fd822e00227839
Lucas Camargo <lucas8camargo@outlook.com>
2021-07-05 22:23:48 -0300

    Métodos do módulo 'Random'

---
+++ Métodos do módulo 'Random'/Métodos do módulo 'Random'.py
@@ -0,0 +1,8 @@
+Import random 
+ 
+random.sample(x, k) - x (lista de itens) k (quantidade de itens sorteados) 
+random.random() - Retorna um numero flutuante entre"0 a 0.9" 
+random.randint(3, 9) - Retorna um numero inteiro aleatorio entre"X e Y" 
+random.shuffle(x) - Mistura os itens de uma lista permanentimente 
+
+
